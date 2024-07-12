6cd325249c4359dbb92bbb5f7bd2f4949c22f3f0
martin3698753 <martinek8425@gmail.com>
2024-07-12 21:20:09 +0200

    ball work

--- ball/ball.c
+++ ball/ball.c
@@ -1,5 +1,5 @@
 #define DIM 3 //int of fail
-#define PI 3.14159
+#define PI 3.141592653589793
 #define RAD 1
 
 #include <stdio.h>
@@ -15,31 +15,42 @@ double square_area(double r)
 {
   return (r*2);
 }
-double volume(int dim, double rad)
+double volume()
 {
-  return ((pow(PI, dim/2)*pow(rad, dim))/tgamma((dim/2)+1));
+  double a = pow(PI, (double)DIM/2) * pow((double)RAD, (double)DIM);
+  double b = tgamma( (double)DIM/2 + (double)1 );
+  return ((double)(a/b));
 }
-
-int main()
+double comp_vol(int N)
 {
-  unsigned int n = 1;
-  unsigned int N = 1000000;
   double r;
   double nball;
-  unsigned int seed = (unsigned int)time(NULL);
-  time_t walltime;
-  srand(seed);
 
   for(int i = 0; i < N; i++) {
     r = 0;
     for(int j = 0; j < DIM; j++) {
       r += pow(rnd(1), 2);
     }
-    if(r <= 1) {
+    if(r <= RAD) {
       nball++;
     }
   }
-  printf("%lf\n", (nball*8)/N);
-  printf("%lf\n", volume(3, 1));
+  return (nball * pow(2, DIM))/N;
+}
+
+int main()
+{
+  unsigned int n = 1;
+  unsigned int N = 1000000;
+  unsigned int seed = (unsigned int)time(NULL);
+  time_t walltime;
+  srand(seed);
+  //printf("%lf\n", pow(PI, (double)1/2));
+  //printf("%lf\n", tgamma((double)1/2 + 1));
+  printf("%lf\n", volume());
+  printf("%lf\n", comp_vol(N));
+
+  //printf("%Computed volume = lf\n", (nball*8)/N);
+  //printf("Actual volume = %lf\n", volume(3, 1));
   return 0;
 }
